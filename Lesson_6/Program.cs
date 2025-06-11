5b0c465cb9786ea9ff9f0b964feb1932398282f7
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-11 12:45:45 +0500

    Task 2

--- Lesson_6/Program.cs
+++ Lesson_6/Program.cs
@@ -6,17 +6,38 @@ namespace PersonDemo
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Firstname = "Jaloliddin";
-            person.Lastname = "Mahkamov";
-            person.Age = 30;
-            person.Address = "Toshkent Yunusobod";
+            //Person person = new Person();
+            //person.Firstname = "Jaloliddin";
+            //person.Lastname = "Mahkamov";
+            //person.Age = 30;
+            //person.Address = "Toshkent Yunusobod";
 
-            string fullName = person.GetFullName();
-            Console.WriteLine($"Full Name: {fullName}");
+            //string fullName = person.GetFullName();
+            //Console.WriteLine($"Full Name: {fullName}");
 
-            int birthYear = person.GetBirthYear();
-            Console.WriteLine($"Birth Year: {birthYear}");
+            //int birthYear = person.GetBirthYear();
+            //Console.WriteLine($"Birth Year: {birthYear}");
+
+
+            //Task 2
+            Rectangle rect = new Rectangle
+            {
+                Width = 5.0,
+                Height = 3.0
+            };
+
+
+            double area1 = rect.GetArea();
+            Console.WriteLine($"Initial dimensions: {rect.Width} × {rect.Height}");
+            Console.WriteLine($"Area: {area1}");
+
+            rect.Width = 10.0;
+            rect.Height = 4.5;
+
+            double area2 = rect.GetArea();
+            Console.WriteLine($"\nUpdated dimensions: {rect.Width} × {rect.Height}");
+            Console.WriteLine($"New area: {area2}");
         }
     }
 }
+
