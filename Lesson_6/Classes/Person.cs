c002ae3252746cb89da48d284ab316abafa78286
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-11 12:42:54 +0500

    Task 1

---
+++ Lesson_6/Classes/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson_6.Classes
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{Firstname} {Lastname}";
+        }
+
+        public int GetBirthYear()
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - Age;
+        }
+    }
+}
---
+++ Lesson_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lesson_6.Classes;
+
+namespace PersonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Firstname = "Jaloliddin";
+            person.Lastname = "Mahkamov";
+            person.Age = 30;
+            person.Address = "Toshkent Yunusobod";
+
+            string fullName = person.GetFullName();
+            Console.WriteLine($"Full Name: {fullName}");
+
+            int birthYear = person.GetBirthYear();
+            Console.WriteLine($"Birth Year: {birthYear}");
+        }
+    }
+}
