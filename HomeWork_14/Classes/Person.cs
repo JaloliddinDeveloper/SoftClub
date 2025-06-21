720e73b4e603d621f2ac2c167730855821e9b211
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-21 12:57:44 +0500

    HomeWork - 14 Implement

---
+++ HomeWork_14/Classes/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeWork_14.Classes
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Person(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public virtual string GetDetails()
+        {
+            return $"Name: {Name}, Address: {Address}";
+        }
+    }
+}
