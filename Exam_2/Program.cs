7e9b6519203490ac9cf3ad26f4215c314272f3aa
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-21 19:31:32 +0500

    Program

--- Exam_2/Program.cs
+++ Exam_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exam_2.ClassesOne;
+
+Student student1 =new Student("Jaloliddin","Shahrisabz","C#",4,100000);
+Student student2 =new Student("Oybek","Kitob","C++",1,100000);
+Student student3 =new Student("Mustafo","Olot","C",2,100000);
+
+Console.WriteLine(student2.GetName());
+Console.WriteLine(student3.GetName());
+Console.WriteLine(student1.GetName());
+
+Console.WriteLine(student2.GetProgram());
+Console.WriteLine(student3.GetProgram());
+Console.WriteLine(student1.GetProgram());
+
+
