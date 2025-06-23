ca636b8e75c8821e70e579bc29bdb144c7eb2d83
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-06-23 20:14:56 +0500

    StudentService Implement

---
+++ Lesson_15/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson_15.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
